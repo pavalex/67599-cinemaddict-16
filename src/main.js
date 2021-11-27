913e9879c9650c8e0ab5eb52e479b2f636b68dea
pavalex <Alex.P79@mail.ru>
2021-11-27 21:42:46 +0800

    Время разбивать камни. Доработки

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import {createSiteMenuTemplate} from './view/site-menu-view.js';
 import {createSiteRankTemplate} from './view/site-rank-view.js';
 import {createSiteSortTemplate} from './view/site-sort-view.js';
+import {createFilmsTemplate} from './view/site-films-view.js';
 import {createSiteCardTemplate} from './view/site-card-view.js';
 import {createButtonShowMoreTemplate} from './view/site-buttonShowMore-view.js';
-import {createPopupTemplate} from './view/site-popup-view.js';
+// import {createPopupTemplate} from './view/site-popup-view.js'; // Раскомментировать для попапа
 import {createFooterDataTemplate} from './view/site-footerData-view.js';
 import {createTopTemplate} from './view/site-top-view.js';
 import {createMostTemplate} from './view/site-most-view.js';
@@ -14,26 +15,34 @@ const TASK_COUNT = 5;
 const siteHeaderElement = document.querySelector('.header');
 const siteHeaderLogoElement = siteHeaderElement.querySelector('.header__logo');
 const siteMainElement = document.querySelector('.main');
-const siteMenuElement = siteMainElement.querySelector('.menu');
-const siteSortElement = siteMainElement.querySelector('.sort-films');
-const siteFilmsElement = siteMainElement.querySelector('.films');
-const siteFilmsListElement = siteFilmsElement.querySelector('.films-list');
-const siteCardElement = siteFilmsElement.querySelector('.films-list__container');
 const siteFooterElement = document.querySelector('.footer');
 const siteFooterStatistics = siteFooterElement.querySelector('.footer__statistics');
-const sitePopupElement = document.querySelector('.popup');
 
 renderTemplate(siteHeaderLogoElement, createSiteRankTemplate(), RenderPosition.AFTEREND);
-renderTemplate(siteMenuElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(siteSortElement, createSiteSortTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSiteSortTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createFilmsTemplate(), RenderPosition.BEFOREEND);
+
+const siteFilmsElement = siteMainElement.querySelector('.films');
+const siteFilmsListElement = siteFilmsElement.querySelector('.films-list');
+const siteFilmsListContainer = siteFilmsElement.querySelector('.films-list__container');
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  renderTemplate(siteCardElement, createSiteCardTemplate(), RenderPosition.BEFOREEND);
+  renderTemplate(siteFilmsListContainer, createSiteCardTemplate(), RenderPosition.BEFOREEND);
 }
 
 renderTemplate(siteFilmsListElement, createButtonShowMoreTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteFilmsElement, createTopTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteFilmsElement, createMostTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteFooterStatistics, createFooterDataTemplate(), RenderPosition.AFTEREND);
-renderTemplate(sitePopupElement, createPopupTemplate(), RenderPosition.BEFOREEND);
+
+/* Попап. Создал секцию с классом, вставил на страницу и отрисовал.
+Пока закомментировал, чтобы не закрывал страницу */
+
+// const popupElement = document.createElement('section');
+// popupElement.classList.add('film-details');
+// siteFooterElement.after(popupElement);
+// const sitePopupElement = document.querySelector('.film-details');
+
+// renderTemplate(sitePopupElement, createPopupTemplate(), RenderPosition.BEFOREEND);
 
